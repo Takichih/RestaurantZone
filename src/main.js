f57d52f6c20dd3d2a4a0ad8eb7e5c59267b9d6cf
Jeremy Genest-Picard <jegep@ulaval.ca>
2024-09-12 19:49:00 -0400

    Installed VuetifyJS + MD icons

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 import { createApp } from "vue";
+
+// Vuetify
+import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css'
+import { createVuetify } from "vuetify";
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+
+// Router
 import { router } from "./router";
+
+// Components
 import App from "./App.vue";
 
-const app = createApp(App);
+const vuetify = createVuetify({
+  components,
+  directives
+});
 
-app.use(router);
-app.mount("#app");
+createApp(App).use(router).use(vuetify).mount("#app");
