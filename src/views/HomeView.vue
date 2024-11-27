c05ac0012de2c984313fbe17daa7fc3e84b3e4e4
dghhjejh <emmanuel.kona.1@ulaval.ca>
2024-11-27 12:36:12 -0500

    il reste juste à rendre les icônes cliquables

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,8 @@
 import { ref, onMounted } from "vue";
 import { useRoute } from "vue-router";
 import RestaurantList from "@/components/HomePage/RestaurantList.vue";
-
+import ModeMap from "@/components/HomePage/ModeMap.vue";
+const tab = ref("one");
 const route = useRoute();
 const search = ref(route.query.search || "");
 
@@ -12,7 +13,25 @@ onMounted(() => {
 </script>
 
 <template>
-  <div>
-    <RestaurantList :initialSearch="search" />
-  </div>
+  <v-card>
+    <v-tabs
+      v-model="tab"
+      align-tabs="center"
+      bg-color="primary"
+    >
+      <v-tab :value="one">Liste de restaurants</v-tab>
+      <v-tab :value="two">Mode Map</v-tab>
+    </v-tabs>
+
+    <v-card-text>
+      <v-tabs-window v-model="tab">
+        <v-tabs-window-item value="one">
+          <RestaurantList :initialSearch="search" />
+        </v-tabs-window-item>
+        <v-tabs-window-item value="two">
+          <ModeMap/>
+        </v-tabs-window-item>
+      </v-tabs-window>
+    </v-card-text>
+  </v-card>
 </template>
