9c92c8da798a2a7a51ad1dfc563fc849af76a7ff
Kevin Vachon <kevin.vachon.1@ulaval.ca>
2024-12-01 15:37:33 -0500

    Retrait du app secret

--- src/config.js
+++ src/config.js
@@ -3,5 +3,4 @@ export const config = {
   debugMode: true,
   SSL: false,
   facebookAppId: "2390755471273805", //https://developers.facebook.com/apps/2390755471273805/settings/basic/
-  facebookAppSecret: "bdc569cae7689762abed5426a101eb02"
 };
